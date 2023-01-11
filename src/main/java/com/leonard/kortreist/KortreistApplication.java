df19d56e71cc6e4ccc165bec190df1f25a02efc2
Leonard Opsal Taklo <leonaro@stud.ntnu.no>
2023-01-11 10:48:13 +0100

    spring boot

---
+++ src/main/java/com/leonard/kortreist/KortreistApplication.java
@@ -0,0 +1,13 @@
+package com.leonard.kortreist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KortreistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KortreistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leonard/kortreist/KortreistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonard.kortreist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KortreistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
